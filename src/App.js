021758a2c7b86a3f31f08364f0b79451c4870441
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-06-13 14:04:42 +0400

    Header and background is completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Header from "./components/Header/Header";
 function App() {
 	return (
 		<div>
+			<img
+				className="background"
+				alt="background"
+				src="assets/background/background.png"
+			/>
 			<Header />
 		</div>
 	);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
+import FormatData from "../../util/FormatData";
+import { CalendarIcon } from "../UI/Icons";
+import styles from "./Header.module.css";
+
 function Header() {
+	const [date, setDate] = useState(new Date());
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDate(new Date());
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<header>
-			<div className="header">
-				<div>logo</div>
-				<div>calendar</div>
+			<div className={styles.container}>
+				<div>
+					<img className={styles.logo} src="assets/logo/Logo.png" alt="logo" />
+				</div>
+				<div className={styles.calendarContainer}>
+					<div className={styles.dateContainer}>
+						<div className={styles.date}>{FormatData.formatDate(date)}</div>
+						<div className={styles.time}>{FormatData.formatTime(date)}</div>
+					</div>
+					<div>
+						<CalendarIcon />
+					</div>
+				</div>
 			</div>
 		</header>
 	);
