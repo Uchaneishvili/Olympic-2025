5ca7972c69dac4e28d0fe069c4b40d7135db6964
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-06-18 15:01:32 +0400

    list is added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,95 @@
 import { Divider } from "../UI/Icons";
 import styles from "./Card.module.css";
 function Card() {
+	const data = [
+		{
+			country: "SLO",
+			flag: (
+				<img className="flag" alt="country-flag" src="flags/Slovenia.png" />
+			),
+			name: "L. Repinc",
+			time: "31:34.07",
+		},
+		{
+			country: "POL",
+			flag: <img className="flag" alt="country-flag" src="flags/Poland.png" />,
+			name: "K. Badacz",
+			time: "31:34.12",
+		},
+		{
+			country: "SVK",
+			flag: (
+				<img className="flag" alt="country-flag" src="flags/Slovakia.png" />
+			),
+			name: "J. Borgula",
+			time: "31:34:20",
+		},
+		{
+			country: "SWE",
+			flag: <img className="flag" alt="country-flag" src="flags/Sweden.png" />,
+			name: "S. Anderson",
+			time: "31:34.39",
+		},
+		{
+			country: "FRA",
+			flag: <img className="flag" alt="country-flag" src="flags/France.png" />,
+			name: "L. Thievent",
+			time: "31:34.59",
+		},
+		{
+			country: "ITA",
+			flag: <img className="flag" alt="country-flag" src="flags/Italy.png" />,
+			name: "E. Mondinelli",
+			time: "31:35.05",
+		},
+		{
+			country: "AUT",
+			flag: <img className="flag" alt="country-flag" src="flags/Austria.png" />,
+			name: "V. Olivier",
+			time: "31:35.10",
+		},
+	];
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>ALPINE SKIING</div>
 			<div className={styles.tableContainer}>
 				<Divider />
-				<div className={styles.innerContainer}></div>
+				<div className={styles.innerContainer}>
+					<div className={styles.table}>
+						<div className={styles.subtitleContainer}>
+							<div className={styles.subtitleInnerContainer}>
+								<div className={styles.dashedLine}></div>
+								<div className={styles.subtitle}>
+									10 km Individual start free (boys)
+								</div>
+								<div className={styles.dashedLine}></div>
+							</div>
+						</div>
+						<div className={styles.list}>
+							{data.map((player, index) => {
+								return (
+									<div className={styles.row} key={index}>
+										<div className={styles.playerInfo}>
+											<div
+												className={`${styles.ranking} ${
+													index === 0 && styles.first
+												} ${index === 1 && styles.second} ${
+													index === 2 && styles.third
+												}`}
+											>
+												<div className={styles.index}>{index + 1}.</div>
+												<div className={styles.flag}>{player.flag}</div>
+											</div>
+											<div className={styles.country}>({player.country})</div>
+											<div className={styles.name}>{player.name}</div>
+										</div>
+										<div className={styles.result}>{player.time}</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
