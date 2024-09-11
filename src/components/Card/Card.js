66c91d99a95a23cbd99270f45c9d3676d13a9fe3
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-09-11 16:40:25 +0400

    rerendering is fixed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,140 +1,137 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { Divider, DownRanking, UpRanking } from "../UI/Icons";
 import styles from "./Card.module.css";
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation, Reorder } from "framer-motion";
 import axios from "axios";
+
+const PlayerRow = React.memo(({ player, index }) => {
+	const controls = useAnimation();
+
+	useEffect(() => {
+		controls.start({
+			opacity: [1, 0.5, 1],
+			y: player.rankChange ? -40 * player.rankChange : 0,
+			x: 0,
+			scale: 1,
+			rotate: 0,
+			transition: {
+				duration: 2,
+				opacity: { times: [0, 0.5, 1], duration: 2 },
+			},
+		});
+	}, [player.rankChange, controls]);
+
+	return (
+		<Reorder.Item
+			className={styles.row}
+			key={player.id}
+			value={player}
+			transition={{ duration: 2 }}
+			animate={controls}
+			initial={{ opacity: 1 }}
+		>
+			<div className={styles.playerInfo}>
+				<div
+					className={`${styles.ranking} ${index === 0 && styles.first} ${
+						index === 1 && styles.second
+					} ${index === 2 && styles.third}`}
+				>
+					<div className={styles.index}>{index + 1}.</div>
+					<div className={styles.flag}>
+						<img
+							className="flag"
+							alt="country-flag"
+							src={`flags/${player.flag}.png`}
+						/>
+					</div>
+				</div>
+				<div className={styles.country}>({player.country})</div>
+				<div className={styles.name}>{player.name}</div>
+				<div className={styles.name}>
+					{player.rankChange > 0 && <UpRanking />}
+					{player.rankChange < 0 && <DownRanking />}
+				</div>
+			</div>
+			<div className={styles.result}>{player.time}</div>
+		</Reorder.Item>
+	);
+});
 
 function Card() {
 	const [data, setData] = useState([]);
 	const [updatedData, setUpdatedData] = useState([]);
-	const [animate, setAnimate] = useState(false);
 	const [isFlipped, setIsFlipped] = useState(false);
 
-	const loadData = async () => {
-		const response = await axios.get(
-			`${process.env.REACT_APP_API_URL}/players`
-		);
-
-		if (response.status === 200) {
-			setData(response.data);
-		}
-	};
-
-	const loadUpdatedData = async () => {
-		const response = await axios.get(
-			`${process.env.REACT_APP_API_URL}/updatedPlayers`
-		);
-
-		if (response.status === 200) {
-			setInterval(() => {
+	const loadData = useCallback(async () => {
+		try {
+			const response = await axios.get(
+				`${process.env.REACT_APP_API_URL}/players`
+			);
+			if (response.status === 200) {
 				setData(response.data);
-			}, 3000);
-			setUpdatedData(response.data);
+			}
+		} catch (error) {
+			console.error("Error loading initial data:", error);
 		}
-	};
-	useEffect(() => {
-		loadUpdatedData().then(() => {
-			setAnimate(true);
-		});
-		loadData();
 	}, []);
 
-	// useEffect(() => {
-	// 	const socket = io(process.env.REACT_APP_WEBSOCKET_URL);
+	const loadUpdatedData = useCallback(async () => {
+		try {
+			const response = await axios.get(
+				`${process.env.REACT_APP_API_URL}/updatedPlayers`
+			);
+			if (response.status === 200) {
+				setUpdatedData(response.data);
+				setTimeout(() => {
+					setData(response.data);
+				}, 3000);
+			}
+		} catch (error) {
+			console.error("Error loading updated data:", error);
+		}
+	}, []);
 
-	// 	socket.on("connect", () => {
-	// 		console.log("Connected to WebSocket server");
+	useEffect(() => {
+		loadData();
+		loadUpdatedData();
 
-	// 		// Request initial data
-	// 		socket.emit("getInitialData");
-	// 	});
+		const intervalId = setInterval(loadUpdatedData, 3000); // Update every 30 seconds
 
-	// 	socket.on("initialData", (initialData) => {
-	// 		setData(initialData);
-	// 		setAnimate(true);
-	// 	});
+		return () => clearInterval(intervalId);
+	}, [loadData, loadUpdatedData]);
 
-	// 	socket.on("updatedData", (newData) => {
-	// 		setData(newData);
-	// 		setUpdatedData(newData);
-	// 	});
+	const commonStyles = useMemo(
+		() => ({
+			padding: "0px",
+			position: "absolute",
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+			flexDirection: "column",
+			backfaceVisibility: "hidden",
+		}),
+		[]
+	);
 
-	// 	socket.on("disconnect", () => {
-	// 		console.log("Disconnected from WebSocket server");
-	// 	});
+	const playerList = useMemo(() => {
+		return data.map((player, index) => {
+			const updatedIndex = updatedData.findIndex((p) => p.id === player.id);
+			const rankChange = updatedIndex !== -1 ? index - updatedIndex : 0;
+			return { ...player, rankChange };
+		});
+	}, [data, updatedData]);
 
-	// 	return () => {
-	// 		socket.disconnect();
-	// 	};
-	// }, []);
-
-	const commonStyles = {
-		position: "absolute",
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-		flexDirection: "column",
-		backfaceVisibility: "hidden",
-	};
-
-	const PlayerRow = ({ player, index }) => {
-		const controls = useAnimation();
-		useEffect(() => {
-			controls.start({
-				opacity: [1, 0.5, 1],
-				y: animate ? -40 * player.rankChange : 0,
-				transition: {
-					duration: 2,
-					opacity: { times: [0, 0.5, 1], duration: 2 },
-				},
-			});
-		}, [player.rankChange, controls]);
-
-		return (
-			<motion.div
-				className={styles.row}
-				key={index}
-				animate={controls}
-				initial={{ opacity: 1 }}
-			>
-				<div className={styles.playerInfo}>
-					<div
-						className={`${styles.ranking} ${index === 0 && styles.first} ${
-							index === 1 && styles.second
-						} ${index === 2 && styles.third}`}
-					>
-						<div className={styles.index}>{index + 1}.</div>
-						<div className={styles.flag}>
-							<img
-								className="flag"
-								alt="country-flag"
-								src={`flags/${player.flag}.png`}
-							/>
-						</div>
-					</div>
-					<div className={styles.country}>({player.country})</div>
-					<div className={styles.name}>{player.name}</div>
-					<div className={styles.name}>
-						{player.rankChange > 0 && <UpRanking />}
-						{player.rankChange < 0 && <DownRanking />}
-					</div>
-				</div>
-				<div className={styles.result}>{player.time}</div>
-			</motion.div>
-		);
-	};
 	return (
 		<div
 			onClick={() => setIsFlipped(!isFlipped)}
-			style={{}}
 			className={styles.mainContainer}
 		>
-			<motion.div
-				style={{
-					...commonStyles,
-				}}
+			<Reorder.Group
+				style={commonStyles}
 				animate={{ rotateY: isFlipped ? 180 : 0 }}
 				transition={{ duration: 0.6 }}
+				values={playerList}
+				onReorder={setData}
 				className={styles.container}
 			>
 				<div className={styles.title}>ALPINE SKIING</div>
@@ -152,24 +149,17 @@ function Card() {
 								</div>
 							</div>
 							<div className={styles.list}>
-								{data.map((player, index) => {
-									const updatedIndex = updatedData.findIndex(
-										(p) => p.id === player.id
-									);
-									player.rankChange = index - updatedIndex;
-
-									return <PlayerRow player={player} index={index} />;
-								})}
+								{playerList.map((player, index) => (
+									<PlayerRow key={player.id} player={player} index={index} />
+								))}
 							</div>
 						</div>
 					</div>
 				</div>
-			</motion.div>
+			</Reorder.Group>
 
 			<motion.div
-				style={{
-					...commonStyles,
-				}}
+				style={commonStyles}
 				initial={{ rotateY: 180 }}
 				animate={{ rotateY: isFlipped ? 0 : 180 }}
 				transition={{ duration: 0.6 }}
